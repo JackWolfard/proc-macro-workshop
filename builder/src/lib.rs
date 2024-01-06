01827afbd4bbe6cbc59cd5628567c8928c670607
Jack Wolfard <31635014+JackWolfard@users.noreply.github.com>
2024-01-06 13:30:57 -0600

    jack(builder): pass test 4

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,6 @@
 use proc_macro2::TokenStream;
 use quote::{format_ident, quote};
-use syn::{parse_macro_input, Data, DeriveInput, Field, Fields};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, LitStr};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -11,6 +11,7 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let fields_decl = fields(&input.data).map(field_decl);
     let fields_none = fields(&input.data).map(field_none);
     let fields_setter = fields(&input.data).map(field_setter);
+    let built_fields = fields(&input.data).map(build_field);
 
     let expanded = quote! {
         impl #name {
@@ -26,11 +27,30 @@ pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
         }
 
         impl #builder {
+            pub fn build(&mut self) -> Result<#name, Box<dyn std::error::Error>> {
+                Ok(#name {
+                    #(#built_fields),*
+                })
+            }
+
             #(#fields_setter)*
         }
     };
 
     expanded.into()
+}
+
+fn build_field(field: &Field) -> TokenStream {
+    if let Some(name) = &field.ident {
+        let err_msg = LitStr::new(
+            &format!("expected '{}' to have been set", name),
+            name.span(),
+        );
+        return quote! {
+            #name: self.#name.clone().ok_or(#err_msg)?
+        };
+    }
+    unimplemented!();
 }
 
 fn field_setter(field: &Field) -> TokenStream {
