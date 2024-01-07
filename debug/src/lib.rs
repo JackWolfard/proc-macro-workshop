995ae7d870025d5ea4ea14f1605312d75998d050
Jack Wolfard <31635014+JackWolfard@users.noreply.github.com>
2024-01-07 11:45:30 -0600

    jack(debug): pass test 2

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,10 +1,58 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Field, Fields, LitStr};
+
+struct Test {
+    x: i32,
+    y: i32,
+}
+
+impl std::fmt::Debug for Test {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("Test")
+            .field("x", &self.x)
+            .field("y", &self.y)
+            .finish()
+    }
+}
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = parse_macro_input!(input as DeriveInput);
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
 
-    quote! {}.into()
+    let name = input.ident;
+    let lit_name = LitStr::new(&name.to_string(), name.span());
+
+    let fields_debug = fields(&input.data).map(field_debug);
+
+    quote! {
+        impl std::fmt::Debug for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                f.debug_struct(#lit_name)
+                    #(#fields_debug)*
+                    .finish()
+            }
+        }
+    }
+    .into()
+}
+
+fn fields(data: &Data) -> impl Iterator<Item = &Field> {
+    match data {
+        Data::Struct(data) => match &data.fields {
+            Fields::Named(fields) => fields.named.iter(),
+            _ => unimplemented!(),
+        },
+        _ => unimplemented!(),
+    }
+}
+
+fn field_debug(field: &Field) -> TokenStream {
+    if let Some(name) = &field.ident {
+        let lit_name = LitStr::new(&name.to_string(), name.span());
+        return quote! {
+            .field(#lit_name, &self.#name)
+        };
+    }
+    unimplemented!();
 }
