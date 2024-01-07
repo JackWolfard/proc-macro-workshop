f99410325ad5bc69566966c6e9dbe73cccbef670
Jack Wolfard <31635014+JackWolfard@users.noreply.github.com>
2024-01-07 11:32:24 -0600

    jack(debug): initial setup

---
+++ debug/WORKLOG.md
@@ -0,0 +1,9 @@
+# Worklog
+
+- `cargo-expand` integration
+  - followed [@tamuratak][gh-tamuratak]'s suggestion
+  - moved all tests from `tests/` to `examples/`
+  - modified `tests/progress.rs` to reflect new paths
+  - now, can expand w/ `cargo expand --example 01-parse` and test with `cargo test --tests`
+
+[gh-tamuratak]: https://github.com/tamuratak
--- debug/tests/progress.rs
+++ debug/tests/progress.rs
@@ -1,12 +1,12 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    //t.pass("tests/01-parse.rs");
-    //t.pass("tests/02-impl-debug.rs");
-    //t.pass("tests/03-custom-format.rs");
-    //t.pass("tests/04-type-parameter.rs");
-    //t.pass("tests/05-phantom-data.rs");
-    //t.pass("tests/06-bound-trouble.rs");
-    //t.pass("tests/07-associated-type.rs");
-    //t.pass("tests/08-escape-hatch.rs");
+    //t.pass("examples/01-parse.rs");
+    //t.pass("examples/02-impl-debug.rs");
+    //t.pass("examples/03-custom-format.rs");
+    //t.pass("examples/04-type-parameter.rs");
+    //t.pass("examples/05-phantom-data.rs");
+    //t.pass("examples/06-bound-trouble.rs");
+    //t.pass("examples/07-associated-type.rs");
+    //t.pass("examples/08-escape-hatch.rs");
 }
