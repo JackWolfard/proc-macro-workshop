7e8646c12f17aafb57ef116d95b9182a5f51cfe3
Jack Wolfard <31635014+JackWolfard@users.noreply.github.com>
2024-01-08 14:10:14 -0600

    jack(seq): initial setup

--- seq/tests/progress.rs
+++ seq/tests/progress.rs
@@ -1,13 +1,13 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    //t.pass("tests/01-parse-header.rs");
-    //t.pass("tests/02-parse-body.rs");
-    //t.compile_fail("tests/03-expand-four-errors.rs");
-    //t.pass("tests/04-paste-ident.rs");
-    //t.pass("tests/05-repeat-section.rs");
-    //t.pass("tests/06-init-array.rs");
-    //t.pass("tests/07-inclusive-range.rs");
-    //t.compile_fail("tests/08-ident-span.rs");
-    //t.pass("tests/09-interaction-with-macrorules.rs");
+    //t.pass("examples/01-parse-header.rs");
+    //t.pass("examples/02-parse-body.rs");
+    //t.compile_fail("examples/03-expand-four-errors.rs");
+    //t.pass("examples/04-paste-ident.rs");
+    //t.pass("examples/05-repeat-section.rs");
+    //t.pass("examples/06-init-array.rs");
+    //t.pass("examples/07-inclusive-range.rs");
+    //t.compile_fail("examples/08-ident-span.rs");
+    //t.pass("examples/09-interaction-with-macrorules.rs");
 }
