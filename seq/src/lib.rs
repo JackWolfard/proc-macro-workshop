fd21035647f1f3ee00c90d395a3e78e4d02b1721
Jack Wolfard <31635014+JackWolfard@users.noreply.github.com>
2024-01-09 09:46:03 -0600

    jack(seq): pass test 1

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,8 +1,35 @@
-use proc_macro::TokenStream;
+use quote::quote;
+// use proc_macro2::TokenStream;
+use syn::parse::{Parse, ParseStream};
+use syn::{braced, parse_macro_input, token, Ident, LitInt, Result, Token};
+
+#[derive(Debug)]
+struct Sequence {
+    ident: Ident,
+    in_token: Token![in],
+    start: LitInt,
+    dotdot_token: Token![..],
+    end: LitInt,
+    brace_token: token::Brace,
+}
+
+impl Parse for Sequence {
+    fn parse(input: ParseStream) -> Result<Self> {
+        let content;
+        Ok(Sequence {
+            ident: input.parse()?,
+            in_token: input.parse()?,
+            start: input.parse()?,
+            dotdot_token: input.parse()?,
+            end: input.parse()?,
+            brace_token: braced!(content in input),
+        })
+    }
+}
 
 #[proc_macro]
-pub fn seq(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn seq(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let sequence = parse_macro_input!(input as Sequence);
 
-    unimplemented!()
+    quote! {}.into()
 }
