c2d1b61f4d4e2dbce622905621c70f4b32f30636
Jack Wolfard <31635014+JackWolfard@users.noreply.github.com>
2024-01-10 15:09:04 -0800

    jack(sorted): initial setup

--- sorted/tests/progress.rs
+++ sorted/tests/progress.rs
@@ -1,12 +1,12 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    //t.pass("tests/01-parse-enum.rs");
-    //t.compile_fail("tests/02-not-enum.rs");
-    //t.compile_fail("tests/03-out-of-order.rs");
-    //t.compile_fail("tests/04-variants-with-data.rs");
-    //t.compile_fail("tests/05-match-expr.rs");
-    //t.compile_fail("tests/06-pattern-path.rs");
-    //t.compile_fail("tests/07-unrecognized-pattern.rs");
-    //t.pass("tests/08-underscore.rs");
+    //t.pass("examples/01-parse-enum.rs");
+    //t.compile_fail("examples/02-not-enum.rs");
+    //t.compile_fail("examples/03-out-of-order.rs");
+    //t.compile_fail("examples/04-variants-with-data.rs");
+    //t.compile_fail("examples/05-match-expr.rs");
+    //t.compile_fail("examples/06-pattern-path.rs");
+    //t.compile_fail("examples/07-unrecognized-pattern.rs");
+    //t.pass("examples/08-underscore.rs");
 }
