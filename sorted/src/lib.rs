57bdb9a8f445a91fefcffe1119bb38aacbdd53ef
Jack Wolfard <31635014+JackWolfard@users.noreply.github.com>
2024-01-10 15:30:01 -0800

    jack(sorted): pass test 2

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,13 +1,30 @@
-use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Item};
+use syn::{parse_macro_input, Error, Item, Result};
 
 #[proc_macro_attribute]
-pub fn sorted(_: TokenStream, input: TokenStream) -> TokenStream {
+pub fn sorted(
+    _: proc_macro::TokenStream,
+    input: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as Item);
 
-    quote! {
-        #input
+    let output = match sorted_impl(input) {
+        Ok(output) => quote! {
+            #output
+        },
+        Err(output) => output.into_compile_error(),
+    };
+
+    output.into()
+}
+
+fn sorted_impl(item: Item) -> Result<Item> {
+    if let Item::Enum(item) = item {
+        Ok(item.into())
+    } else {
+        Err(Error::new_spanned(
+            item,
+            "#[sorted] cannot be applied to things other than enum",
+        ))
     }
-    .into()
 }
