d76fd9d55565a935eddca306943676e6c184106e
Jack Wolfard <31635014+JackWolfard@users.noreply.github.com>
2024-01-10 15:17:57 -0800

    jack(sorted): pass test 1

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -1,9 +1,13 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, Item};
 
 #[proc_macro_attribute]
-pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
-    let _ = args;
-    let _ = input;
+pub fn sorted(_: TokenStream, input: TokenStream) -> TokenStream {
+    let input = parse_macro_input!(input as Item);
 
-    unimplemented!()
+    quote! {
+        #input
+    }
+    .into()
 }
--- sorted/tests/progress.rs
+++ sorted/tests/progress.rs
@@ -1,7 +1,7 @@
 #[test]
 fn tests() {
     let t = trybuild::TestCases::new();
-    //t.pass("examples/01-parse-enum.rs");
+    t.pass("examples/01-parse-enum.rs");
     //t.compile_fail("examples/02-not-enum.rs");
     //t.compile_fail("examples/03-out-of-order.rs");
     //t.compile_fail("examples/04-variants-with-data.rs");
